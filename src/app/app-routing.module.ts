c34140905601711221ed932847d04d92b52e2818
Belinda Lei <belinda.lei95@gmail.com>
2020-04-06 11:21:15 -0500

    added routes and airtable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
+import { SafetyMeasuresComponent } from './safety-measures/safety-measures.component'
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes = [
+  { path: 'safety-measures', component: SafetyMeasuresComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'safety-measures', pathMatch: 'full' }
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SafetyMeasuresComponent } from './safety-measures/safety-measures.component';
+import { AboutComponent } from './about/about.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SafetyMeasuresComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
