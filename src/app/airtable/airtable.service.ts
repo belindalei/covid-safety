a6d12d926caf2438c6fa5f70cfdeb7525e47d5e7
Belinda Lei <belinda.lei95@gmail.com>
2020-04-10 11:50:42 -0500

    testing out the environment.ts file

--- src/app/airtable/airtable.service.ts
+++ src/app/airtable/airtable.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import { AIRTABLE_API_KEY } from '../../../env'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AirtableService {
+
+  private _url: string =
+    'https://api.airtable.com/v0/appMzVVjn9eVT5NBG/Scores?maxRecords=3&view=Company%20Score%20Comparison';
+
   constructor(private http: HttpClient) {}
 
-  // **Trying to get the method working here after storing my api key in .env. The request isn't going through though...not sure if it's because I'n using the wrong request for angular. This is the link request that works for ruby
+  // **Trying to get the method working here after storing my api key in .env. The request isn't going through though...not sure if it's because I'n using the wrong request for angular. This is the link request that works for
   //https://api.airtable.com/v0/appMzVVjn9eVT5NBG/Companies\, headers={'Authorization': "Bearer #{ENV['AIRTABLE_API_KEY']}"
-  // getAPIData() {
-  //   return this.http.get();
-  // }
 
+
+  getAPIData() {
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer' + 
+    })
+
+    return this.http.get(this._url, {
+      headers: header
+    });
+  }
 }
